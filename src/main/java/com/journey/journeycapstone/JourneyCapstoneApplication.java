7d4b717e9c6dcaa9b5111bc1dd592aa6387e4a9c
Lorne Cousins <lorne.cousins88@gmail.com>
2022-06-01 12:16:09 -0500

    setup files

---
+++ src/main/java/com/journey/journeycapstone/JourneyCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.journey.journeycapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JourneyCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/journey/journeycapstone/JourneyCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journey.journeycapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
