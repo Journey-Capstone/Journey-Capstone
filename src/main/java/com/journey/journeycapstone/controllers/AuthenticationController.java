a3db771c9852649a7b7ab85d222ba6a4331645bc
Yasmine Rico <ricolyasmine@gmail.com>
2022-06-05 19:21:57 -0500

    working on Api call

--- src/main/java/com/journey/journeycapstone/controllers/AuthenticationController.java
+++ src/main/java/com/journey/journeycapstone/controllers/AuthenticationController.java
@@ -1,7 +1,11 @@
 package com.journey.journeycapstone.controllers;
 
+import com.journey.journeycapstone.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthenticationController {
@@ -11,5 +15,10 @@ public class AuthenticationController {
             return "users/login";
         }
 
+        @PostMapping("/login")
+        public String loginSubmit(@ModelAttribute User user, Model model){
+
+        return "/profile";
+        }
 
 }
