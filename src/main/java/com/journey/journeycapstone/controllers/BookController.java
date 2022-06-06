002974110b80f4f3c07402181631833b131c034b
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-06-06 12:18:17 -0500

    moving work to different branch

--- src/main/java/com/journey/journeycapstone/controllers/BookController.java
+++ src/main/java/com/journey/journeycapstone/controllers/BookController.java
@@ -1,9 +1,10 @@
 package com.journey.journeycapstone.controllers;
 
+import com.journey.journeycapstone.models.Review;
+import com.journey.journeycapstone.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 
--- src/main/java/com/journey/journeycapstone/repositories/ReviewRepository.java
+++ src/main/java/com/journey/journeycapstone/repositories/ReviewRepository.java
@@ -1,4 +1,7 @@
 package com.journey.journeycapstone.repositories;
 
+import com.journey.journeycapstone.models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ReviewRepository {
 }
