86dba0566aa326c639c8383aa05ff93fb89a50ef
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-06-06 16:49:00 -0500

    completed adding review form to single book page

--- src/main/java/com/journey/journeycapstone/controllers/BookController.java
+++ src/main/java/com/journey/journeycapstone/controllers/BookController.java
@@ -2,6 +2,7 @@ package com.journey.journeycapstone.controllers;
 
 import com.journey.journeycapstone.models.Review;
 import com.journey.journeycapstone.models.User;
+import com.journey.journeycapstone.repositories.ReviewRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,16 +10,41 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 
 public class BookController {
+    private ReviewRepository reviewDao;
+
+    public BookController(ReviewRepository reviewDao){
+        this.reviewDao = reviewDao;
+    }
+
 
     @GetMapping("/main")
     public String mainPage(){
         return "main/main";
     }
 
+//    @GetMapping("/books")
+//    public String singleBook(){
+//        return "main/singleBook";
+//    }
+
+    //review form
     @GetMapping("/books")
-    public String singleBook(){
+    public String reviewsForm(Model model){
+        model.addAttribute("review", new Review());
         return "main/singleBook";
     }
+    @PostMapping("/books")
+    public String reviewsSubmit(@ModelAttribute Review review, Model model){
+        // This is what was added to get it to work
+        // needed to save the user with the userDoa
+        Review newReview = reviewDao.save(review);
+        // Calling the new variable to be added
+        model.addAttribute("review", newReview);
+        return "redirect:/books";
+    }
+    //review form
+
+
 
     @GetMapping("/catalog")
     public String catalogPage(){
