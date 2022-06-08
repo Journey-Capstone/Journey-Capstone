7167137b2a5aeee6027a6aee76d8b3f13672406e
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-06-08 15:01:47 -0500

    added usernames to reviews shown and welcome message to main page

--- src/main/java/com/journey/journeycapstone/controllers/BookController.java
+++ src/main/java/com/journey/journeycapstone/controllers/BookController.java
@@ -24,6 +24,8 @@ public class BookController {
     @GetMapping("/main")
     public String mainPage(Model model){
         model.addAttribute("reviews", reviewDao.findAll());
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user",user);
         return "main/main";
     }
 
