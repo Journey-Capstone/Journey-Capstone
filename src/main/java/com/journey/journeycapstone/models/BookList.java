aff4b97882864f7a1825e167bfdb7587170bf7a6
Lorne Cousins <lorne.cousins88@gmail.com>
2022-06-13 09:58:36 -0500

    added multiple users to seeder

--- src/main/java/com/journey/journeycapstone/models/BookList.java
+++ src/main/java/com/journey/journeycapstone/models/BookList.java
@@ -1,78 +1,78 @@
-package com.journey.journeycapstone.models;
-
-import javax.persistence.*;
-import java.util.List;
-
-@Entity
-@Table(name = "bookLists")
-public class BookList {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    //references user(id)
-    @OneToOne
-    private User owner;
-
-    //references book(id)
-//    @OneToMany
-//    private List<Book> books;
-
-    //references status(id)
-    @OneToOne
-    private Status status;
-
-    private String bookTitle;
-
-    public String getBookTitle() {
-        return bookTitle;
-    }
-
-    public void setBookTitle(String bookTitle) {
-        this.bookTitle = bookTitle;
-    }
-
-    public BookList(){}
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-//    public long getBook_id() {
-//        return book_id;
+//package com.journey.journeycapstone.models;
+//
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@Entity
+//@Table(name = "bookLists")
+//public class BookList {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private long id;
+//
+//    //references user(id)
+//    @OneToOne
+//    private User owner;
+//
+//    //references book(id)
+////    @OneToMany
+////    private List<Book> books;
+//
+//    //references status(id)
+////    @OneToOne
+////    private Status status;
+//
+//    private String bookTitle;
+//
+//    public String getBookTitle() {
+//        return bookTitle;
 //    }
 //
-//    public void setBook_id(long book_id) {
-//        this.book_id = book_id;
-//    }
-
-
-    public User getOwner() {
-        return owner;
-    }
-
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
-
-//    public List<Book> getBooks() {
-//        return books;
+//    public void setBookTitle(String bookTitle) {
+//        this.bookTitle = bookTitle;
 //    }
 //
-//    public void setBooks(List<Book> books) {
-//        this.books = books;
+//    public BookList(){}
+//
+//    public long getId() {
+//        return id;
 //    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-}
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+////    public long getBook_id() {
+////        return book_id;
+////    }
+////
+////    public void setBook_id(long book_id) {
+////        this.book_id = book_id;
+////    }
+//
+//
+//    public User getOwner() {
+//        return owner;
+//    }
+//
+//    public void setOwner(User owner) {
+//        this.owner = owner;
+//    }
+//
+////    public List<Book> getBooks() {
+////        return books;
+////    }
+////
+////    public void setBooks(List<Book> books) {
+////        this.books = books;
+////    }
+//
+////    public Status getStatus() {
+////        return status;
+////    }
+////
+////    public void setStatus(Status status) {
+////        this.status = status;
+////    }
+//}
--- src/main/java/com/journey/journeycapstone/models/Review.java
+++ src/main/java/com/journey/journeycapstone/models/Review.java
@@ -22,6 +22,10 @@ public class Review {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
     public Review(){
     }
 
